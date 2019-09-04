fe748bb132f58b0ccca06ebb20b4555d3c01a0c8
Mabel Montano <27mabel@gmail.com>
2019-09-04 19:23:01 -0400

    removing vulnerability

--- docs/setVariables.bat
+++ docs/setVariables.bat
@@ -7,8 +7,8 @@ ECHO Begin Set in variables...
 if not exist "C:\Project\myExchangeProject" mkdir C:\Project\myExchangeProject
 
 SET GOPATH=C:\Project\myExchangeProject
-SET SLACKTOKEN=xoxp-735488424065-735488424737-744894194389-2fd39509c7061e8face78c8e14cdfd87
-SET EXCHANGETOKEN=b2e3d360a5c775a403d9ddff35e33cbd
+SET SLACKTOKEN=TOKEN HERE
+SET EXCHANGETOKEN=TOKEN HERE
 
 ECHO Done...
 
